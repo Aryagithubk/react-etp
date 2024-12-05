b8bf8196449f09cbf950b51425a52abce7be7230
Aryagithubk <aryasanya55@gmail.com>
2024-12-05 10:42:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UserList from "./UserList";
+import PostList from "./PostList";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>API Data Fetching with Custom Hook</h1>
+      <UserList />
+      <PostList />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import useFetch from "./useFetch";
+import "./App.css";
+
+const UserList = () => {
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  if (loading) return <div className="loading">Loading...</div>;
+  if (error) return <div className="error">Error: {error}</div>;
+
+  return (
+    <div className="user-list">
+      <h2>User List</h2>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id} className="user-item">
+            <h3>{user.name}</h3>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
